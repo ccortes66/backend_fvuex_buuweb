15adfb3b6831c68268d4e2b7c1d9df796f48c2cf
ccortes66 <ccortes66@misena.edu.co>
2023-11-09 09:24:35 -0500

    shorting links show modify

--- src/main/java/com/vuex/example/vuex/persistences/DTO/Links/ShowLinks.java
+++ src/main/java/com/vuex/example/vuex/persistences/DTO/Links/ShowLinks.java
@@ -7,15 +7,14 @@ import com.vuex.example.vuex.persistences.document.LinkDocument;
 public record ShowLinks(
    String id, 
    String longLink,
-   String shortLink,  
-   String user
+   String shortLink
 ) 
 {
     public ShowLinks(LinkDocument document)
     {
         this(document.get_id(),
              document.getLongLink(), 
-             document.getShortLink(), 
-             document.getDocument().getEmail());
+             document.getShortLink()
+             );
     }
 }
